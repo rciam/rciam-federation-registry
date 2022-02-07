efb890b2fc115636628fe2ba4e7c62e0421bf923
Andreas Kozadinos <koza-sparrow@hotmail.com>
2022-02-07 15:19:02 +0200

    RCIAM-820

--- federation-registry-backend-api/JavaScript/db/sql/service_state/add.sql
+++ federation-registry-backend-api/JavaScript/db/sql/service_state/add.sql
@@ -1,3 +1,3 @@
-INSERT INTO service_state (id,state,deployment_type,last_edited,created_at)
-VALUES (${id},${state},${deployment_type},current_timestamp,current_timestamp)
+INSERT INTO service_state (id,state,deployment_type,last_edited)
+VALUES (${id},${state},${deployment_type},current_timestamp)
 RETURNING *
