38f97b5a2817a49e4449c0b4a5e05395c81de903
Andreas Kozadinos <koza-sparrow@hotmail.com>
2023-03-13 22:58:45 +0200

    Metadata Url Validation Namespaces

--- federation-registry-backend-api/JavaScript/routes/util_routes.js
+++ federation-registry-backend-api/JavaScript/routes/util_routes.js
@@ -11,7 +11,6 @@ parser.on('error', function(err) { console.log('Parser error', err); });
 router.get('/metadata_info',async (req,res,next)=>{
     try{
       let url = decodeURIComponent(req.query.metadata_url);
-      var data = '';
       axios.get(url,{timeout:2000})
       .then(response => {
         let data = response.data;
@@ -31,20 +30,23 @@ router.get('/metadata_info',async (req,res,next)=>{
         }
         try{
           if(result){
+            let ns = extractNamespace(result);
             let entity_id, requested_attributes; 
             let supported_attributes=[];
             let unsupported_attributes= [];
-            if(!(result.hasOwnProperty('md:EntityDescriptor')&&result['md:EntityDescriptor'].hasOwnProperty('md:SPSSODescriptor'))){
+            if(!(result.hasOwnProperty(ns+'EntityDescriptor')&&result[ns+'EntityDescriptor'].hasOwnProperty(ns+'SPSSODescriptor'))){
               res.statusMessage = "XML contained in the metadata is not Valid."
               res.status(404).send();
             }
-            try{                  
-              entity_id=result['md:EntityDescriptor']['_attributes'].entityID;  
+            try{               
+              entity_id=result[ns+'EntityDescriptor']['_attributes'].entityID;  
             }
-            catch(err){};
+            catch(err){
+              console.log(err);
+            };
             try{
-              requested_attributes = result['md:EntityDescriptor']['md:SPSSODescriptor']['md:AttributeConsumingService']['md:RequestedAttribute'];                 
-              if(requested_attributes){
+              requested_attributes = result[ns+'EntityDescriptor'][ns+'SPSSODescriptor'][ns+'AttributeConsumingService'][ns+'RequestedAttribute'];                 
+              if(requested_attributes&&requested_attributes.length>0){
                 requested_attributes.forEach((attribute,index)=>{
                   requested_attributes[index] = {
                     friendly_name:requested_attributes[index]._attributes.FriendlyName,
@@ -57,15 +59,12 @@ router.get('/metadata_info',async (req,res,next)=>{
                 unsupported_attributes = requested_attributes.filter(e=> default_supported_attributes.every(x=>x.name!==e.name));
               }
             }
-            catch(err){
-              if(entity_id){
-                res.status(200).send({entity_id,supported_attributes:[],unsupported_attributes:[],metadata_url:url});
-              }
-            }
+            catch(err){}
             res.status(200).send({entity_id, supported_attributes:supported_attributes, unsupported_attributes:unsupported_attributes,metadata_url:url});
           }  
         }
         catch(err){
+          console.log(err);
           res.statusMessage = "Could not load Service properties from Metadata Url."
           res.status(404).send();
         }
@@ -79,7 +78,6 @@ router.get('/metadata_info',async (req,res,next)=>{
 
     }
     catch(err){
-      console.log(err);
       res.statusMessage = "Could not get response from Metadata Url"
       res.status(404).send();
     }
@@ -87,6 +85,22 @@ router.get('/metadata_info',async (req,res,next)=>{
   })
 
 
+
+function extractNamespace(xml) {
+  let regex = /^(.*):EntityDescriptor/g; 
+  let match;
+
+  for (var attribute in xml) {
+    match = !match&&regex.exec(attribute)
+  }
+  if(match&&match[1]){
+    return match[1]+":";
+  }
+  else{
+    return "";
+  }
+};
+  
 
 
 module.exports = router;
