4b1fb4c0e6ed764d72900914ca20f13977313218
Andreas Kozadinos <koza-sparrow@hotmail.com>
2026-06-05 15:51:19 +0300

    Service Integration Notifications BUG: RCIAM-767

--- federation-registry-backend-api/JavaScript/db/sql/service_petition_details/getTicketInfo.sql
+++ federation-registry-backend-api/JavaScript/db/sql/service_petition_details/getTicketInfo.sql
@@ -1,10 +1,53 @@
-SELECT service_name,integration_environment,type,preferred_username as reviewer_username, email as reviewer_email,requester_username,requester_email,reviewed_at,protocol,petition.tenant
-  FROM
- (SELECT reviewed_at,protocol,service_name,integration_environment,type,preferred_username as requester_username, email as requester_email,reviewer,petition.tenant FROM
-    (
-      SELECT *
-      FROM
-      (SELECT reviewed_at,service_name,integration_environment,type,requester,reviewer,protocol,tenant,Rank() over (Partition BY service_id ORDER BY reviewed_at DESC) AS RANK FROM service_petition_details WHERE service_id IN (${ids:csv}) ${tenant_selector:raw} AND reviewed_at IS NOT NULL)rs WHERE Rank <= 1
-    ) as petition
-  LEFT JOIN user_info ON petition.requester = user_info.sub) as petition
-LEFT JOIN user_info ON petition.reviewer = user_info.sub
+SELECT
+  petition.service_name,
+  petition.integration_environment,
+  petition.type,
+  reviewer_info.preferred_username AS reviewer_username,
+  reviewer_info.email AS reviewer_email,
+  petition.requester_username,
+  petition.requester_email,
+  petition.reviewed_at,
+  petition.protocol,
+  petition.tenant
+FROM (
+  SELECT
+    petition.reviewed_at,
+    petition.protocol,
+    petition.service_name,
+    petition.integration_environment,
+    petition.type,
+    requester_info.preferred_username AS requester_username,
+    requester_info.email AS requester_email,
+    petition.reviewer,
+    petition.tenant
+  FROM (
+    SELECT *
+    FROM (
+      SELECT
+        reviewed_at,
+        service_name,
+        integration_environment,
+        type,
+        requester,
+        reviewer,
+        protocol,
+        tenant,
+        RANK() OVER (
+          PARTITION BY service_id
+          ORDER BY reviewed_at DESC
+        ) AS rank
+      FROM service_petition_details
+      WHERE
+        service_id IN (${ids:csv})
+        ${tenant_selector:raw}
+        AND reviewed_at IS NOT NULL
+    ) rs
+    WHERE rank <= 1
+  ) AS petition
+  LEFT JOIN user_info AS requester_info
+    ON petition.requester = requester_info.sub
+   AND petition.tenant = requester_info.tenant
+) AS petition
+LEFT JOIN user_info AS reviewer_info
+  ON petition.reviewer = reviewer_info.sub
+ AND petition.tenant = reviewer_info.tenant;
