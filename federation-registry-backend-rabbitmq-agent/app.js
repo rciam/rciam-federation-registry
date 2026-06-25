93d91a7f2063816f5a5a285ea0ba53487cb2390f
Jiří Prokop <jprokop.ofc@gmail.com>
2026-06-25 14:13:29 +0000

    fix: stale messages after error
    After restart, old messages should be flushed and refreshed from rabbit. Otherwise, they're marked as stale by rabbit and error is thrown.

--- federation-registry-backend-rabbitmq-agent/app.js
+++ federation-registry-backend-rabbitmq-agent/app.js
@@ -155,6 +155,12 @@ function scheduleRestart() {
     runIntervalTask = null;
   }
 
+  if (sendResultTask) {
+    clearInterval(sendResultTask);
+    sendResultTaskRunning = false;
+  }
+  ResultMessageBatch.clear();
+
   if (connection) {
     connection.close().catch(() => {});
     connection = null;
